7fd05c3d91cb2082e61b3771cde63455903886ab
Moondip Konwar <moondipkonwar9@gmail.com>
2026-02-22 16:13:50 +0000

    Input and output fields

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="p-4 flex flex-col gap-8">
+  <!-- Input  -->
+  <form action="" class="flex flex-col gap-2">
+    <label for="input" class="font-bold"
+      >Enter text or Morse code (separated by spaces):</label
+    >
+    <textarea
+      name=""
+      id="input"
+      class="border border-gray-800 h-[5rem] p-2 rounded-lg w-full"
+      placeholder="Enter something to get started..."
+    ></textarea>
+  </form>
+
+  <!-- Output -->
+  <div>
+    <h3 class="font-bold">Output:</h3>
+    <textarea
+      name=""
+      id="output"
+      class="border border-gray-800 h-[5rem] p-2 rounded-lg w-full disabled"
+      placeholder="Enter something to get started..."
+    ></textarea>
+  </div>
+</main>
