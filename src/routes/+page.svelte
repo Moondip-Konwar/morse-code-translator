09c6eb8a63c5a16cc8cb0cd22327299bacfa97a8
Moondip Konwar <moondipkonwar9@gmail.com>
2026-02-22 16:22:27 +0000

    Translated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,60 @@
+<script lang="ts">
+  let morse_map: Record<string, string> = {
+    A: "• -",
+    B: "- • • •",
+    C: "- • - •",
+    D: "- • •",
+    E: "•",
+    F: "• • - •",
+    G: "- - •",
+    H: "• • • •",
+    I: "• •",
+    J: "• - - -",
+    K: "- • -",
+    L: "• - • •",
+    M: "- -",
+    N: "- •",
+    O: "- - -",
+    P: "• - - •",
+    Q: "- - • -",
+    R: "• - •",
+    S: "• • •",
+    T: "-",
+    U: "• • -",
+    V: "• • • -",
+    W: "• - -",
+    X: "- • • -",
+    Y: "- • - -",
+    Z: "- - • •",
+    "1": "• - - - -",
+    "2": "• • - - -",
+    "3": "• • • - -",
+    "4": "• • • • -",
+    "5": "• • • • •",
+    "6": "- • • • •",
+    "7": "- - • • •",
+    "8": "- - - • •",
+    "9": "- - - - •",
+    "0": "- - - - -",
+    " ": "/",
+  };
+
+  let input_text = "";
+  let output_text = "";
+
+  function handle_submit(event: SubmitEvent) {
+    event.preventDefault();
+    output_text = "";
+
+    for (let c = 0; c < input_text.length; c++) {
+      output_text += morse_map[input_text[c].toUpperCase()];
+    }
+  }
+</script>
+
 <main class="p-4 flex flex-col gap-8">
   <!-- Input  -->
-  <form action="" class="flex flex-col gap-2">
+  <form action="" class="flex flex-col gap-2" onsubmit={handle_submit}>
     <label for="input" class="font-bold"
       >Enter text or Morse code (separated by spaces):</label
     >
@@ -9,7 +63,9 @@
       id="input"
       class="border border-gray-800 h-[5rem] p-2 rounded-lg w-full"
       placeholder="Enter something to get started..."
+      bind:value={input_text}
     ></textarea>
+    <button type="submit">Convert</button>
   </form>
 
   <!-- Output -->
@@ -21,6 +77,7 @@
       class="border border-gray-400 h-[5rem] p-2 rounded-lg w-full disabled"
       placeholder="Enter something to get started..."
       disabled
+      bind:value={output_text}
     ></textarea>
   </div>
 </main>
